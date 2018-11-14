323bc30575d56e5025604f63d04caf3bb0f06fc1
cenjie <a291183649@gmail.com>
2018-11-14 11:45:54 +0800

    2018/11/14

---
+++ LeetCode/src/algorithms/dynamicProgramming/easy/climbingStairs/MainClass.java
@@ -0,0 +1,8 @@
+package algorithms.dynamicProgramming.easy.climbingStairs;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.climbStairs(2));
+	}
+}
---
+++ LeetCode/src/algorithms/dynamicProgramming/easy/climbingStairs/Solution2.java
@@ -0,0 +1,5 @@
+package algorithms.dynamicProgramming.easy.climbingStairs;
+
+public class Solution2 {
+
+}
