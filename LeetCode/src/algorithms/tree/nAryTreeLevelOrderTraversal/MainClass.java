4c95eb1a6adaee08261c2a32f7397a31a5d5ad47
cenjie <a291183649@gmail.com>
2018-11-22 12:03:40 +0800

    2018/11/22

--- LeetCode/src/algorithms/tree/nAryTreeLevelOrderTraversal/MainClass.java
+++ LeetCode/src/algorithms/tree/nAryTreeLevelOrderTraversal/MainClass.java
@@ -1,5 +1,20 @@
 package algorithms.tree.nAryTreeLevelOrderTraversal;
 
+import java.util.List;
+
+
 public class MainClass {
 
 }
+
+class Node {
+	 public int val;
+	 public List<Node> children;
+	
+	 public Node() {}
+	
+	 public Node(int _val,List<Node> _children) {
+	     val = _val;
+	     children = _children;
+	 }
+};
--- LeetCode/src/algorithms/tree/nAryTreeLevelOrderTraversal/Solution.java
+++ LeetCode/src/algorithms/tree/nAryTreeLevelOrderTraversal/Solution.java
@@ -1,5 +1,26 @@
 package algorithms.tree.nAryTreeLevelOrderTraversal;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
 
+public class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+    	List<List<Integer>> res = new ArrayList<>();
+    	if(root == null) return res;
+    	Queue<Node> queue = new LinkedList<>();
+    	queue.add(root);
+    	while(!queue.isEmpty()) {
+    		int size = queue.size();
+    		List<Integer> list = new ArrayList<>();
+    		while(size-- != 0) {
+    			Node node = queue.poll();
+    			for(Node children : node.children) queue.add(children);
+    			list.add(node.val);
+    		}
+    		res.add(list);
+    	}
+    	return res;
+    }
 }
