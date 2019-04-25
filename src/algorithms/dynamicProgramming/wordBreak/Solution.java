10cd36752610012f63b84a0208ecc6a7c42ecf6b
cenjie <a291183649@gmail.com>
2019-04-25 09:37:12 +0800

    ok

--- src/algorithms/dynamicProgramming/wordBreak/Solution.java
+++ src/algorithms/dynamicProgramming/wordBreak/Solution.java
@@ -1,5 +1,19 @@
 package algorithms.dynamicProgramming.wordBreak;
 
-public class Solution {
+import java.util.List;
 
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i = 1; i <= s.length(); i++) {
+        	for(int j = 0; j < i; j++) {
+        		if(dp[j] && wordDict.contains(s.substring(j, i))) {
+        			dp[i] = true;
+        			break;
+        		}
+        	}
+        }
+        return dp[s.length()];
+    }
 }
