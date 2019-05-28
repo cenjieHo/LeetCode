0e8c2ce562887e636dcfce000c036c1eaa7cd851
cenjie <a291183649@gmail.com>
2019-05-28 11:32:38 +0800

    2019/5/28

--- src/algorithms/NAC/coinChange/Solution.java
+++ src/algorithms/NAC/coinChange/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
         for(int i = 0; i < dp.length; i++) {
-        	dp[i] = Integer.MAX_VALUE;
+        	dp[i] = amount+1;
         }
         dp[0] = 0;
         for(int i = 1; i <= amount; i++) {
@@ -17,6 +17,6 @@ public class Solution {
         		}
         	}
         }
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
